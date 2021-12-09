f09b5e1fbe701976df076bd2f25266ce074b5e43
gopalakrishnanv708 <gopalakrishnanv708@gmail.com>
2021-12-09 14:29:47 +0530

    Plan changes

--- src/components/Plan.js
+++ src/components/Plan.js
@@ -7,98 +7,102 @@ export default function Plan() {
 
     return (
         <div className="col-12  bg-dark justify-content-center">
-        <div className="col-lg-4 col-md-4 col-12 p-4">
-            <div className="mt-3 text-center">
-                <img src={Image} alt="some text" className="w-30 h-30"/>
+            <div className="col-lg-4 col-md-4 col-12 p-4">
+                <div className="mt-3 text-center">
+                    <img src={Image} alt="some text" className="w-30 h-30" />
+                </div>
+
+                <div className="mt-2 text-center">
+                    <h3 className="text-white">Create Your</h3>
+                    <h3 className="text-white"><span className="plans-striv3-text-color">Striv3</span> Plan</h3>
+                </div>
+
+                <div className="col-12 mt-1 text-center">
+                    <img src={PlanButton} alt="some text" />
+                </div>
+
+                <div className="col-12 mt-2 d-flex justify-content-around">
+                    <div className="col-6">
+                        <button className="btn plans-meditation-button">Meditation Goal:</button>
+                    </div>
+                    <div className="col-4">
+                        <span className="text-white">Need help <br /> planning?(vid)</span>
+                    </div>
+                </div>
+
+                <div className="col-12 mt-3 text-white plan-how-long">
+                    <span>How many times/wk do you want to <br /> meditate? For how Long?</span>
+                </div>
+
+                <div className="col-12 mt-4 d-flex">
+                    <div className="col-6 ms-2">
+                        <input type="number" className="plan-input-box" /><span className="text-white ms-2">Days/Wk</span>
+                    </div>
+                    <div className="col-6">
+                        <input type="number" className="col-6 plan-input-box" /><span className="text-white ms-2">Minutes</span>
+                    </div>
+                </div>
+
+                <div className="mt-3 text-white plan-how-long">
+                    <span>What Days & Time work best for you?<br /> (Check all that apply)</span>
+                </div>
+
+                <div className="col-12 mt-4 d-flex">
+                    <div className="col-3 ms-2">
+                        <input type="checkbox" className="plan-input-box" /><span className="text-white ms-2">Mon</span>
+                    </div>
+                    <div className="col-3">
+                        <input type="checkbox" className="plan-input-box" /><span className="text-white ms-2">Tues</span>
+                    </div>
+                    <div className="col-3">
+                        <input type="checkbox" className="plan-input-box" /><span className="text-white ms-2">Wed</span>
+                    </div>
+                    <div className="col-3">
+                        <input type="checkbox" className="plan-input-box" /><span className="text-white ms-2">Thurs</span>
+                    </div>
+                </div>
+
+                <div className="col-12 mt-4 d-flex">
+                    <div className="col-3 ms-2">
+                        <input type="checkbox" className="plan-input-box" /><span className="text-white ms-2">Fri</span>
+                    </div>
+                    <div className="col-3">
+                        <input type="checkbox" className="plan-input-box" /><span className="text-white ms-2">Sat</span>
+                    </div>
+                    <div className="col-3">
+                        <input type="checkbox" className="plan-input-box" /><span className="text-white ms-2">Sun</span>
+                    </div>
+                </div>
+
+                <div className="col-12 mt-3 text-white plan-how-long">
+                    <span>Time of Day:</span>
+                </div>
+
+                <div className="col-12 mt-2 d-flex">
+                    <div className="col-4 ms-2">
+                        <input type="checkbox" className="plan-input-box" /><span className="text-white ms-2">Morning</span>
+                    </div>
+                    <div className="col-4">
+                        <input type="checkbox" className="plan-input-box" /><span className="text-white ms-2">Noon</span>
+                    </div>
+                    <div className="col-4">
+                        <input type="checkbox" className="plan-input-box" /><span className="text-white ms-2">Evening</span>
+                    </div>
+                </div>
+
+                <div className="mt-3 text-white plan-how-long">
+                    <span>What other goals would you like to see?<br /> offered to track with your striv3 plan?</span>
+                </div>
+
+                <div className="mt-2">
+                    <textarea placeholder="(List out Here)" className="plan-text-area-class"></textarea>
+                </div>
+
             </div>
 
-            <div className="mt-2 text-center">
-              <h3 className="text-white">Create Your</h3>
-               <h3 className="text-white"><span className="plans-striv3-text-color">Striv3</span> Plan</h3>
+            <div className="m-2 text-center">
+                <button className="strivbut btn btn-success">Continue</button>
             </div>
-
-            <div className="mt-1 text-center">
-                <img src={PlanButton} alt="some text"/>
-            </div>
-
-            <div className="mt-2 d-flex justify-content-around">
-                <div>
-                <button className="plans-meditation-button">Meditation Goal:</button>
-                </div>
-                <div>
-                 <span className="text-white">Need help <br/> planning?(vid)</span>
-                </div>
-            </div>
-
-            <div className="mt-3 text-white plan-how-long">
-                <span>How many times/wk do you want to <br/> meditate? For how Long?</span>
-            </div>
-
-            <div className="mt-4 d-flex justify-content-around">
-                <div>
-                    <input type="text" className="plan-input-box"/><span className="text-white ml-5">Days/Wk</span>
-                </div>
-                <div>
-                    <input type="text" className="plan-input-box"/><span className="text-white ml-5">Minutes</span>
-                </div>
-            </div>
-
-            <div className="mt-3 text-white plan-how-long">
-                <span>What Days & Time work best for you?<br/> (Check all that apply)</span>
-            </div>
-
-            <div className="mt-4 d-flex justify-content-around">
-               <div>
-                   <input type="text" className="plan-input-box"/><span className="text-white ml-5">Mon</span>
-               </div>
-                <div>
-                    <input type="text" className="plan-input-box"/><span className="text-white ml-5">Tues</span>
-                </div>
-                <div>
-                    <input type="text" className="plan-input-box"/><span className="text-white ml-5">Wed</span>
-                </div>
-                <div>
-                    <input type="text" className="plan-input-box"/><span className="text-white ml-5">Thurs</span>
-                </div>
-            </div>
-
-            <div className="mt-4 d-flex">
-                <div>
-                    <input type="text" className="plan-input-box"/><span className="text-white ml-5">Fri</span>
-                </div>
-                <div>
-                    <input type="text" className="plan-input-box"/><span className="text-white ml-5">Sat</span>
-                </div>
-                <div>
-                    <input type="text" className="plan-input-box"/><span className="text-white ml-5">Sun</span>
-                </div>
-            </div>
-
-            <div className="mt-3 text-white plan-how-long">
-                <span>Time of Day:</span>
-            </div>
-
-            <div className="mt-2 d-flex">
-                <div>
-                    <input type="text" className="plan-input-box"/><span className="text-white ml-5">Morning</span>
-                </div>
-                <div>
-                    <input type="text" className="plan-input-box"/><span className="text-white ml-5">Noon</span>
-                </div>
-                <div>
-                    <input type="text" className="plan-input-box"/><span className="text-white ml-5">Evening</span>
-                </div>
-            </div>
-
-            <div className="mt-3 text-white plan-how-long">
-                <span>What other goals would you like to see?<br/> offered to track with your striv3 plan?</span>
-            </div>
-
-            <div className="mt-2">
-                <textarea placeholder="(List out Here)" className="plan-text-area-class"></textarea>
-            </div>
-
         </div>
-    </div>
     )
 }
--- src/components/User.js
+++ src/components/User.js
@@ -1,8 +1,8 @@
 import './Design.css'
 import { useState, useEffect } from 'react';
-import {Container} from 'react-bootstrap'
+import { Container } from 'react-bootstrap'
 import axios from "axios";
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import defaultAvatar from "../images/avatar.png"
 
 const userParameters = {
@@ -18,20 +18,20 @@ export default function User() {
 
     useEffect(() => {
 
-       if(localStorage.getItem("token")){
-           axios.get('/users/' + localStorage.getItem("user_id"), {
-               headers: {
-                   'Content-Type': 'application/json',
-                   Accept: "*/*",
-                   Authorization: `Bearer ${localStorage.getItem('token')}`
-               }
-           }).then(res =>{
-               setUser(res.data.data.user)
-           })
-       }
-       else {
-           history("/login")
-       }
+        if (localStorage.getItem("token")) {
+            axios.get('/users/' + localStorage.getItem("user_id"), {
+                headers: {
+                    'Content-Type': 'application/json',
+                    Accept: "*/*",
+                    Authorization: `Bearer ${localStorage.getItem('token')}`
+                }
+            }).then(res => {
+                setUser(res.data.data.user)
+            })
+        }
+        else {
+            history("/login")
+        }
     }, [localStorage.getItem("user_id")]);
 
     const history = useNavigate();
@@ -61,66 +61,99 @@ export default function User() {
                     <a href="/users" className="nav-bar-brand">Striv3r</a>
                 </div>
 
-                <button className="btn btn-sm btn-danger logout-button" onClick={handleLogout}>Logout</button>
+
             </nav>
 
             <Container fluid>
-              <div className="container mt-3">
+                <div className="col-12 mt-3">
 
-                  <div className="row">
-                     <div className="col-md-6 col-sm-6">
-                         <h4 className="text-muted">Profile Pic</h4>
-                         <img src={defaultAvatar} alt="some text" className="default-profile-pic mt-2"/>
-                     </div>
-                  </div>
+                    <div className="col-12 col-sm-4 bg-white p-2 d-flex">
+                        <div className="col-4">
+                            <img src={defaultAvatar} alt="some text" className="default-profile-pic" />
+                        </div>
 
-                  <div className="row mt-3">
-                     <div className="col-md-3 col-sm-3">
-                         <strong className="text-white">Name</strong>
-                     </div>
-                      <div className="col-md-3 col-sm-3 mt-sm-1">
-                          <simple className="text-white">{user.name}</simple>
-                      </div>
-                  </div>
+                        <div className="col-8">
+                            <div>
+                                <strong className="text-dark h3">{user.name}</strong>
+                            </div>
+                            <div className="mt-1">
+                                <simple className="text-dark">{user.zip}</simple>
+                            </div>
+                            <div className="mt-1 d-inline-flex">
+                                <div>
+                                    <simple className="text-dark">Reward Points</simple>
+                                </div>
+                                <div className="ms-1">
+                                    <simple className="text-dark">Referral Points</simple>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
 
-                  <div className="row mt-3">
-                      <div className="col-md-3 col-sm-3">
-                          <strong className="text-white">Email</strong>
-                      </div>
-                      <div className="col-md-3 col-sm-3 mt-sm-1">
-                         <simple className="text-white">{user.email}</simple>
-                      </div>
-                  </div>
+                    <div className="col-12 col-sm-4 p-2 user-cus-div">
+                        <div className="">
+                            <strong className="text-dark"># wks in row</strong>
+                        </div>
+                    </div>
 
-                  <div className="row mt-3">
-                      <div className="col-md-3 col-sm-3">
-                          <strong className="text-white">Phone</strong>
-                      </div>
-                      <div className="col-md-3 col-sm-3 mt-sm-1">
-                          <simple className="text-white">{user.phone}</simple>
-                      </div>
-                  </div>
+                    <div className="col-12 col-sm-4 p-2 user-cus-div-border">
+                        <div className="">
+                            <strong className="text-white">Quote / Tip of the Day</strong>
+                        </div>
+                    </div>
 
-                  <div className="row mt-3">
-                      <div className="col-md-3 col-sm-3">
-                          <strong className="text-white">Zip</strong>
-                      </div>
-                      <div className="col-md-3 col-sm-3 mt-sm-1">
-                          <simple className="text-white">{user.zip}</simple>
-                      </div>
-                  </div>
+                    <div className="col-12 col-sm-4 p-2 user-cus-div-border">
+                        <div className="col-12 d-inline-flex" >
+                            <div className="col-6">
+                                <strong className="text-white h3">Current Goals:</strong>
+                            </div>
+                            <div className="col-3">
 
-                  <div className="row mt-3">
-                      <div className="col-md-3 col-sm-3">
-                          <strong className="text-white">Referral</strong>
-                      </div>
-                      <div className="col-md-3 col-sm-3 mt-sm-1">
-                          <simple className="text-white">{user.referral}</simple>
-                      </div>
-                  </div>
+                            </div>
+                            <div className="col-3">
+                                <strong className="text-white">Edit Goals</strong>
+                            </div>
+                        </div>
+                        <div className="col-12">
+                            <strong className="text-white">My goal is to meditate __ x / week for __ minutes / session</strong>
+                        </div>
+                    </div>
 
-              </div>
+                    <div className="col-12 col-sm-4 p-2 user-cus-div-border">
+                        <div className="col-12 d-inline-flex" >
+                            <div className="col-6">
+                                <strong className="h3 t-colour">Striv3r Plan:</strong>
+                            </div>
+                            <div className="col-3">
+
+                            </div>
+                            <div className="col-3">
+                                <strong className="text-white">Edit Plan</strong>
+                            </div>
+                        </div>
+                        <div className="col-12">
+                            <strong className="t-colour">Week 1:</strong><br />
+                            <strong className="text-white">Medidate 1x this week for 5 min.</strong><input type="checkbox" className="plan-input-box ms-1" />
+                        </div>
+                        <div className="col-12 ms-2">
+                            <strong className="t-colour">Week 2:</strong><br />
+                            <strong className="text-white">Medidate 1x this week for 5 min.</strong><input type="checkbox" className="plan-input-box ms-1" />
+                        </div>
+                        <div className="col-12 ms-4">
+                            <strong className="t-colour">Week 3:</strong><br />
+                            <strong className="text-white">Medidate 1x this week for 5 min.</strong><input type="checkbox" className="plan-input-box ms-1" />
+                        </div>
+                    </div>
+
+
+                </div>
+                <nav className="navbar navbar-light bg-dark justify-content-between">
+
+                    <button className="btn btn-sm btn-danger logout-button" onClick={handleLogout}>Logout</button>
+                    <button className="btn btn-sm btn-danger logout-button">Contact</button>
+                </nav>
             </Container>
+
         </div>
     );
 }
