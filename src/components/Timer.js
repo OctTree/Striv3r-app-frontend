c2c602f50e51045771b6e8f14551017dd03b359b
kishorekce124 <kishorekce124@gmail.com>
2021-12-17 14:59:35 +0530

    fixed changes in timer

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -74,7 +74,6 @@ export default function Timer() {
         const getSeconds = `0${(timer % 60)}`.slice(-2)
         const minutes = `${Math.floor(timer / 60)}`
         const getMinutes = `0${minutes % 60}`.slice(-2)
-        const getHours = `0${Math.floor(timer / 3600)}`.slice(-2)
 
         return `${getMinutes} : ${getSeconds}`
     }
